d0a6d2ade1f4cf64b95ab149551bd965b01b1664
Aaron Spear <aspear@vmware.com>
2015-09-18 09:33:10 -0600

    changed commented only to version 2

--- labotomize-vms.ps1
+++ labotomize-vms.ps1
@@ -2,7 +2,7 @@
 .LABEL
 Labotomize VMs
 .DESCRIPTION
-Performs a Hard or Soft kill of a hung Virtual Machine.
+Performs a Hard or Soft kill of a hung Virtual Machine.  This is version 2.
 #>
 
 param
